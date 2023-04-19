61d8de98ba6f516e69731c4e324a201ac449a189
Espilozzini <131219074+Espilozzini@users.noreply.github.com>
2023-04-18 22:30:18 -0300

    Add files via upload

---
+++ color.h
@@ -0,0 +1,20 @@
+#ifndef COLOR2_H_
+#define COLOR2_H_
+
+void colorPrint(char text[], int mode);
+
+#define BLACK   0
+#define RED     1
+#define GREEN   2
+#define YELLOW  3
+#define BLUE    4
+#define MAGENTA 5
+#define CYAN    6
+#define WHITE   7
+
+#define NORMAL  0
+#define BOLD    8
+#define UNDERLINE 16
+#define INVERSE 24
+
+#endif /* COLOR2_H_ */
